9e65ae25d0cb71a2850316dd4c4d5942aca75f5f
Travis Joseph <travisjoseph@sfs-wifi-dhcp-10-143-232-238.wifi.sfsu.edu>
2017-11-02 14:47:14 -0700

    Now correctly deals with propper noun lookups and cammelCase like words.

--- app1.py
+++ app1.py
@@ -2,19 +2,29 @@ import json
 from difflib import get_close_matches
 
 def lookUp(word):
-    word.lower()
-    if word in data:                                     #query is an exact match to key
-        return data[word]
-    elif len(get_close_matches(word, data.keys())) > 0:
-        for i in get_close_matches(word, data.keys()):
+    propperNoun = word.capitalize()
+    lowerCase = word.lower()
+    #lower case match
+    if lowerCase in data:
+        return data[lowerCase]
+    #propper noun match
+    elif propperNoun in data:
+        return data[propperNoun]
+    #similar lower case
+    elif len(get_close_matches(lowerCase, data.keys())) > 0:
+        for i in get_close_matches(lowerCase, data.keys()):
+            if input("Did you mean %s? Type y or n: " %i).lower() == "y":
+                return data[i]
+    #similar propper noun
+    elif len(get_close_matches(propperNoun, data.keys())) > 0:
+        for i in get_close_matches(propperNoun, data.keys()):
             if input("Did you mean %s? Type y or n: " %i).lower() == "y":
                 return data[i]
     return -1
 #END lookUp
 
 data = json.load(open("data.json"))
-word = input("Word: ")
-definitions = lookUp(word)
+definitions = lookUp(input("Word: "))
 
 if definitions == -1:
     print("Word does not exist. Please try again.")
